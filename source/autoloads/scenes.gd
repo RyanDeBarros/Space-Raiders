c6f710f0f4f7265b610af76b1e369f68cc26fb52
RyanDeBarros <ryan2004db@gmail.com>
2024-03-10 23:10:46 -0400

    Removed redundant constants from scenes.gd

--- source/autoloads/scenes.gd
+++ source/autoloads/scenes.gd
@@ -3,15 +3,6 @@ extends Node
 
 const EXPLOSION_SCENE := preload("res://source/explosion/explosion.tscn")
 
-const PLAYER_PROJECTILE_CONFIG = {
-	"basic": "res://txt/projectile_info/basic.json"
-}
-
-const ENEMY_PROJECTILE_CONFIG = {
-	"basic": "res://txt/projectile_info/basic_slow.json"
-}
-
-
 const PROJECTILES = {
 	"basic": preload("res://source/projectile/basic_shot.tscn")
 }
